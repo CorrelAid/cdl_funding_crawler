f0a97e7cfc76ce849d0d0f7a6ae6c079234773ef
Malte Kyhos <mkyhos@protonmail.com>
2025-09-27 03:59:21 +0200

    feat(troubleshoot-counts): update script, collect individual records

--- troubleshooting.py
+++ troubleshooting.py
@@ -1,16 +1,21 @@
+import os
+import pdb
 import re
-from typing import Optional
-from tqdm import tqdm
+from urllib.parse import parse_qs, urlparse
 
 import polars as pl
 from bs4 import BeautifulSoup
-from requests import Session, HTTPError
+from requests import HTTPError, Session
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 
-BASE_URL = "https://www.foerderdatenbank.de/"
-ENTRY_URL = "SiteGlobals/FDB/Forms/Suche/Expertensuche_Formular.html?filterCategories=FundingProgram"
-HEADERS = {}
+BASE_URL = "https://www.foerderdatenbank.de"
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36",
+    "Sec-Ch-Ua": '"Not=A?Brand";v="24", "Chromium";v="',
+    "Connection": "keep-alive",
+    "Cache-Control": "no-cache",
+}
 
 
 def get_session():
@@ -31,79 +36,126 @@ def get_session():
     return session
 
 
-def get_enumeratable_url_template(session: Session) -> str:
-    """
-    Creates an URL template for iterating over the overview pagination
-    The returned template will contain:
-    - the filters set with the entrypoint url
-    - some query param for session stuff (yielded from their backend)
-    - and a `{page}` tag that can be interpolated subsequently
-    """
-    resp = session.get(BASE_URL + ENTRY_URL)
-    resp.raise_for_status()
+class FDBSearchClient:
+    def __init__(self):
+        self.base_url = BASE_URL
+        self.endpoint_search = (
+            "/SiteGlobals/FDB/Forms/Suche/Expertensuche_Formular.html"
+        )
+        self._fdb_resource_id = None
+        self._fdb_input = None
+        self.params = {}
 
-    html = resp.text
-    if not html:
-        raise ValueError("No html found")
+    def init_session(self):
+        self.params.update(
+            {
+                "filterCategories": "FundingProgram",
+                "sortOrder": "title_text_sort asc",  # Note: the "+" in the visible URL in the browser would represent a space.
+                "submit": "Suchen",
+            }
+        )
+        self.session = get_session()
 
-    soup = BeautifulSoup(html, "html.parser")
-    link_obj = soup.find("a", class_="page")
-    if not link_obj:
-        raise ValueError("No page link found")
+        # Fetch gtp part
+        res = self.session.get(self.base_url + self.endpoint_search, params=self.params)
+        res.raise_for_status()
 
-    link = link_obj["href"]
-    link_template = re.sub(r"(_list%\d+D)\d+", r"\g<1>{page}", link)
-    return BASE_URL + link_template
+        html = res.text
+        if not html:
+            raise ValueError("no html found")
 
+        soup = BeautifulSoup(html, "lxml")
 
-def fetch_page_meta(session: Session, url_template: str, p: int) -> dict:
-    url = url_template.format(page=str(p))
-    print(f"Fetching data for '{url}'")
+        link_obj = soup.find("a", class_="page")
+        if not link_obj:
+            raise ValueError("No page link found")
 
-    resp = session.get(url)
-    resp.raise_for_status()
-    html = resp.text
-    if not html:
-        raise ValueError(f"No html found for page {p}")
+        link = link_obj["href"]
+        gtp_part = parse_qs(urlparse(self.base_url + link).query)["gtp"][0]
+        gtp_template = re.sub(r"(_list%\d+D)\d+", r"\g<1>{page}", gtp_part)
+        self._fdb_gtp_template = gtp_template
 
-    soup = BeautifulSoup(html, "html.parser")
-    record = {
-        "page": p,
-        "hits_count": 0,
-        "records_on_page": 0,
-    }
-    # Extract some metadata
+    def get_overview_page(self, p: int):
+        params = self.params.copy()
 
-    hits_count_obj = soup.find("span", {"id": "hits--count"})
+        cache_file = f"overview_cache/{p}.html"
 
-    if hits_count_obj:
-        record["hits_count"] = int(hits_count_obj.text.replace(".", ""))
+        if os.path.isfile(cache_file):
+            with open(cache_file, "r") as file:
+                html = file.read()
+        else:
+            # append gtp param only for pages > 1 -> otherwise the server fails the request.
+            if p > 1:
+                gtp = self._fdb_gtp_template.format(page=str(p))
+                params = params | {"gtp": gtp}
 
-    entries_obj_list = soup.find_all(
-        "div", class_="card card--horizontal card--fundingprogram"
-    )
-    if entries_obj_list:
-        record["records_on_page"] = len(entries_obj_list)
+            res = self.session.get(self.base_url + self.endpoint_search, params=params)
+            res.raise_for_status()
+            html = res.text
+            with open(f"overview_cache/{p}.html", "w") as file:
+                file.write(html)
 
-    return record
+        rec = self._parse_overview_page(html)
+        rec["page"] = p
+        return rec
+
+    def get_overview_pages(self) -> pl.DataFrame:
+        p_cursor = 1
+        records = []
+
+        while True:
+            print(f"Processing page {p_cursor}")
+            rec = self.get_overview_page(p_cursor)
+
+            if rec["records_on_page"] == 0:
+                break
+
+            records.append(rec)
+            p_cursor += 1
+
+        return pl.DataFrame(records)
+
+    def _parse_overview_page(self, html: str) -> dict:
+        soup = BeautifulSoup(html, "html.parser")
+        record = {
+            "hits_count": 0,
+            "records_on_page": 0,
+            "entries": [],
+        }
+        # Extract some metadata
+        hits_count_obj = soup.find("span", {"id": "hits--count"})
+
+        if hits_count_obj:
+            record["hits_count"] = int(hits_count_obj.text.replace(".", ""))
+
+        entries_obj_list = soup.find_all(
+            "div", class_="card card--horizontal card--fundingprogram"
+        )
+        if entries_obj_list:
+            record["records_on_page"] = len(entries_obj_list)
+
+            # extract some additional details from the entries
+            entry_detail_list = []
+            for entry_obj in entries_obj_list:
+                erec = {}
+                title_obj = entry_obj.find("p", class_="card--title")
+                if not title_obj:
+                    continue
+
+                entry_url = title_obj.find("a").get("href")
+                entry_title = title_obj.find("span", class_="link--label").text
+                entry_title = entry_title.strip()
+
+                entry_detail_list.append(
+                    {"title": entry_title, "url": self.base_url + entry_url}
+                )
+            record["entries"] = entry_detail_list
+        return record
 
 
 if __name__ == "__main__":
-    sess = get_session()
-    url_template = get_enumeratable_url_template(sess)
-
-    records = []
-    p_cursor = 1
-    while True:
-        try:
-            rec = fetch_page_meta(sess, url_template=url_template, p=p_cursor)
-            records.append(rec)
-            p_cursor += 1
-            if rec["records_on_page"] == 0:
-                break
-        except HTTPError as e:
-            print(f"got error: {e}")
-            break
-
-    data = pl.DataFrame(records)
-    data.write_csv("troubleshooting.csv")
+    client = FDBSearchClient()
+    client.init_session()
+    data = client.get_overview_pages()
+    data.write_parquet("troubleshooting.parquet")
+    print(data)
