5f887aa3bd4a5c09cbf1cc3dd29a22ecee43245f
vmfelso <vmfelso@users.noreply.github.com>
2025-10-06 05:25:10 +0200

    Quick analysis

--- selenium_exploration/analyze_scraping.py
+++ selenium_exploration/analyze_scraping.py
@@ -9,11 +9,15 @@ if __name__ == "__main__":
     for page, trial_results in results.items():
         print(f"Page: {page}")
         for result in trial_results:
-            if result is not None:
-                del result["count"]
-            else:
+            if result is None:
                 print(f"No result page {page}")
 
+            else:
+                if (
+                    "https://www.foerderdatenbank.de/FDB/Content/DE/Foerderprogramm/Land/Schleswig-Holstein/wohnprojekte-gruendungsfonds.html"
+                    in result["urls"]
+                ):
+                    print(f"Found in page: {page}")
         for diff in list(dictdiffer.diff(trial_results[0], trial_results[1])):
             print(diff)
 
@@ -25,3 +29,9 @@ if __name__ == "__main__":
 
         for diff in list(dictdiffer.diff(trial_results[0], trial_results[4])):
             print(diff)
+
+    for result in results[16]:
+        print(result["urls"])
+
+    print(set(results[16][2]["urls"]) - set(results[16][0]["urls"]))
+    print(set(results[16][0]["urls"]) - set(results[16][2]["urls"]))
