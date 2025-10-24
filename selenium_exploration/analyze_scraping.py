dae39f88a045461c18b4b264c8a842e57ca020aa
vmfelso <vmfelso@users.noreply.github.com>
2025-10-24 08:38:34 +0200

    Quick analysis

--- selenium_exploration/analyze_scraping.py
+++ selenium_exploration/analyze_scraping.py
@@ -1,37 +1,30 @@
+from collections import Counter
+import dictdiffer
 from pathlib import Path
 import pickle
-import dictdiffer
 
 if __name__ == "__main__":
     with (Path(__file__).parent / "data.pkl").open("rb") as fp:
-        results = pickle.load(fp)
+        meta_results = pickle.load(fp)
 
-    for page, trial_results in results.items():
-        print(f"Page: {page}")
-        for result in trial_results:
-            if result is None:
-                print(f"No result page {page}")
+    for driver, results in meta_results.items():
+        for page, trial_results in results.items():
+            print(f"Page: {page}")
+            for result in trial_results:
+                if result is None:
+                    print(f"No result page {page}")
 
-            else:
-                if (
-                    "https://www.foerderdatenbank.de/FDB/Content/DE/Foerderprogramm/Land/Schleswig-Holstein/wohnprojekte-gruendungsfonds.html"
-                    in result["urls"]
-                ):
-                    print(f"Found in page: {page}")
-        for diff in list(dictdiffer.diff(trial_results[0], trial_results[1])):
-            print(diff)
+            for trial_result in trial_results[1:]:
+                for diff in list(dictdiffer.diff(trial_results[0], trial_result)):
+                    print(diff)
 
-        for diff in list(dictdiffer.diff(trial_results[0], trial_results[2])):
-            print(diff)
-
-        for diff in list(dictdiffer.diff(trial_results[0], trial_results[3])):
-            print(diff)
-
-        for diff in list(dictdiffer.diff(trial_results[0], trial_results[4])):
-            print(diff)
+    print([len(set(result["urls"])) for result in results[16]])
+    print([len(result["urls"]) for result in results[16]])
+    print([len(set(result["titles"])) for result in results[16]])
+    print([len(result["titles"]) for result in results[16]])
+    print(set(results[16][1]["urls"]) - set(results[16][2]["urls"]))
+    print(set(results[16][1]["titles"]) - set(results[16][2]["titles"]))
 
     for result in results[16]:
-        print(result["urls"])
-
-    print(set(results[16][2]["urls"]) - set(results[16][0]["urls"]))
-    print(set(results[16][0]["urls"]) - set(results[16][2]["urls"]))
+        print(Counter(result["urls"]))
+        print(Counter(result["titles"]))
--- selenium_exploration/scraping.py
+++ selenium_exploration/scraping.py
@@ -1,7 +1,7 @@
-import random
 import time
 from pathlib import Path
 import pickle
+import random
 
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
@@ -17,12 +17,12 @@ class SeleniumScraper:
     @staticmethod
     def get_url(page_number: int):
         return (
-            f"https://www.foerderdatenbank.de/"
-            f"SiteGlobals/FDB/Forms/Suche/"
-            f"Startseitensuche_Formular.html?"
-            f"gtp=%2526816beae2-d57e-4bdc-b55d-392bc1e17027_list%253D{page_number}"
+            f"https://www.foerderdatenbank.de/SiteGlobals/FDB/Forms/Suche/Foederprogrammsuche_Formular.html?"
+            f"input_=23adddb0-dcf7-4e32-96f5-93aec5db2716&gtp=%2526816beae2-d57e-4bdc-b55d-392bc1e17027_list%253D{page_number}"
             f"&submit=Suchen"
+            f"&resourceId=0065e6ec-5c0a-4678-b503-b7e7ec435dfd"
             f"&filterCategories=FundingProgram"
+            f"&pageLocale=de"
         )
 
     @staticmethod
@@ -82,6 +82,7 @@ class SeleniumScraper:
             page_data["curr_filter"] = filter_data
 
             urls = []
+            titles = []
             search_result_container = self.driver.find_element(
                 By.CLASS_NAME, "searchresult"
             )
@@ -93,7 +94,11 @@ class SeleniumScraper:
                 urls.append(
                     card_title.find_element(By.TAG_NAME, "a").get_attribute("href")
                 )
+                titles.append(
+                    card_title.find_element(By.CLASS_NAME, "link--label").text
+                )
             page_data["urls"] = urls
+            page_data["titles"] = titles
 
             pagination = self.driver.find_element(By.CLASS_NAME, "pagination")
             final_page = (
@@ -115,22 +120,33 @@ class SeleniumScraper:
 
 
 if __name__ == "__main__":
-    chrome_service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=chrome_service)
+    results = {}
+    num_runs = 10
+    num_pages = 254
+    for browser, browser_manager in [(webdriver.Chrome, ChromeDriverManager)]:
+        browser_text = str(browser)
+        browser_service = Service(browser_manager().install())
+        driver = browser(service=browser_service)
 
-    scraper = SeleniumScraper(driver)
+        scraper = SeleniumScraper(driver)
 
-    results = {page: [] for page in range(1, 254)}
+        results[browser_text] = {page: [] for page in range(1, num_pages)}
+        results[browser_text]["page_orders"] = []
 
-    for run_idx in range(5):
-        pages = list(range(1, 254))
-        random.shuffle(pages)
-        for page_number in pages:
-            results[page_number].append(scraper.read_page(page_number))
-            scraper.jitter_time_out()
+        for run_idx in range(num_runs):
+            pages = list(range(1, num_pages))
+            if run_idx != num_runs - 1:
+                random.shuffle(pages)
+            results[browser_text]["page_orders"].append(pages)
 
-        with (Path(__file__).parent / f"data_{run_idx}.pkl").open("wb") as fp:
-            pickle.dump(results, fp)
+            for page_number in pages:
+                results[browser_text][page_number].append(
+                    scraper.read_page(page_number)
+                )
+                scraper.jitter_time_out()
+
+            with (Path(__file__).parent / f"data_{run_idx}.pkl").open("wb") as fp:
+                pickle.dump(results, fp)
 
     with (Path(__file__).parent / "data.pkl").open("wb") as fp:
         pickle.dump(results, fp)
